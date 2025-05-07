f45b2332831358b66d9176df8b422c56a10f1bb1
jh.cho <jh.cho@kthd.co.kr>
2025-05-07 18:03:44 +0900

    사이드바 작업중

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header className='bg-blue-600 text-white p-4 flex justify-between'>
-        <h1 className='text-xl font-bold'>Hellow</h1>
+        <h1 className='text-xl font-bold'>금강산도식수경</h1>
         <div>
             <button>로그인</button>
         </div>
--- src/components/common/LeftSide.jsx
+++ src/components/common/LeftSide.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const LeftSide = () => {
+
+  // LeftSideBar visible State
+  const [isLeftSideBarVisible, setLeftSideBarVisible] = useState(JSON.parse(localStorage.isLeftSideBarVisible) ?? true);
+
+  // LeftSideBar visible toggle
+  const toggleLeftSideBar = () => {
+    setLeftSideBarVisible(prevState => !prevState);
+  }
+
+  // LeftSideBar save isLeftSideBarVisible localStorage
+  useEffect(() => {
+    localStorage.isLeftSideBarVisible = isLeftSideBarVisible;
+  }, [isLeftSideBarVisible])
+
   return (
-    <aside className="w-48 bg-gray-100 p-2 border-r">
-      <button>⬅ 메뉴</button>
-    </aside>
+    <div className='relative'>
+      <button className='absolute bg-gray-100 z-50 w-8' onClick={toggleLeftSideBar}>{isLeftSideBarVisible ? '⬅' : '➡'}</button>
+
+      <aside className={`transition-all duration-300 ease-in-out ${isLeftSideBarVisible ? 'opacity-100 translate-x-0 w-[12vw]' : 'opacity-0 -translate-x-full w-0'} bg-gray-100 border-r h-full`}>
+        <div className='p-2'>
+          <div className='text-right mt-8 border-b-2'></div>
+        </div>
+      </aside>
+    </div>
   )
 }
 
